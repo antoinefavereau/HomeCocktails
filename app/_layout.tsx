4b4104eae84147550fbf44594cfb216f47fda98b
anatholyb1 <anatholyb@gmail.com>
2025-09-10 18:39:26 +0200

    feat: integrate Clerk for authentication and Convex for data management
    - Added Clerk dependencies for authentication handling in the app.
    - Implemented sign-in and sign-up screens using Clerk's API.
    - Created an authentication layout to manage user sessions.
    - Added a sign-out button component for user logout functionality.
    - Introduced environment variables for Clerk and Convex configurations.
    - Generated Convex API and data model files for backend integration.
    - Updated package.json with new dependencies and devDependencies.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
-import { Stack } from "expo-router";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { Slot } from "expo-router";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+const convexQueryClient = new ConvexQueryClient(convex);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryKeyHashFn: convexQueryClient.hashFn(),
+      queryFn: convexQueryClient.queryFn(),
+    },
+  },
+});
+convexQueryClient.connect(queryClient);
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <QueryClientProvider client={queryClient}>
+          <Slot />
+        </QueryClientProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
